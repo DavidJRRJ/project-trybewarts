2989aa4bc611c69967310e4ef3322695f3501d76
David Junior <david.lourenco94@gmail.com>
2022-05-05 19:32:08 -0300

    botão dinâmico do requisito 16 v2

--- script.js
+++ script.js
@@ -17,14 +17,12 @@ const checkbox = document.querySelector("#agreement");
 const txt = document.querySelector("#txt");
 
 checkbox.addEventListener('change', function() {
-  if (this.checked) {
-    console.log("Checkbox is checked..");
+  if (this.checked) {    
     let btn = document.createElement('button');
     btn.id = 'submit-btn';
     btn.innerHTML = 'Enviar'
     txt.appendChild(btn);
   } else {
-    console.log("Checkbox is not checked..");
     let btn = document.querySelector("#submit-btn");
     txt.removeChild(btn);
   }
