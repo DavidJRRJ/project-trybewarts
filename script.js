1a4183ecf788d7776c52bf6abb2472aafa31899a
Luan Victor <luanvictor2108@gmail.com>
2022-05-05 19:48:58 -0300

    resovendo item 18

--- script.js
+++ script.js
@@ -2,14 +2,29 @@ let buttonLogin = document.getElementById('button1');
 buttonLogin.addEventListener('click' , login);
 let email = document.getElementById('email');
 let senha = document.getElementById('senha');
+let submitButton = document.getElementById('submit-btn');
+let checkbox = document.getElementById('agreement')
+checkbox.addEventListener('click',checaBox);
+let contador = document.getElementById('counter');
+submitButton.disabled = true;
+count();
+
+function checaBox(){
+submitButton.disabled = false;
+}
 
 function login(event){
    event.preventDefault();
    if (email.value == 'tryber@teste.com' && senha.value == '123456'){
     alert('Olá, Tryber!');
-}
-else {
+    }
+    else {
     alert('Email ou senha inválidos.')
+    }
 }
+
+function count() {
+    contador.innerText = '500'
 }
+
 
