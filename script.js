059a0332857a97254686efa48833a22d71a927a7
Luan Victor <luanvictor2108@gmail.com>
2022-05-05 20:22:27 -0300

    resolvendo erros de linter

--- script.js
+++ script.js
@@ -1,33 +1,32 @@
-let buttonLogin = document.getElementById('button1');
-buttonLogin.addEventListener('click' , login);
-let email = document.getElementById('email');
-let senha = document.getElementById('senha');
-let submitButton = document.getElementById('submit-btn');
-let checkbox = document.getElementById('agreement')
-checkbox.addEventListener('click',checaBox);
-let contador = document.getElementById('counter');
-let textarea = document.getElementById('textarea');
+const buttonLogin = document.getElementById('button1');
+const email = document.getElementById('email');
+const senha = document.getElementById('senha');
+const submitButton = document.getElementById('submit-btn');
+const checkbox = document.getElementById('agreement');
+const contador = document.getElementById('counter');
+const textarea = document.getElementById('textarea');
 submitButton.disabled = true;
-textarea.addEventListener('keyup' , count);
-count();
 
-function checaBox(){
-submitButton.disabled = false;
+function checaBox() {
+  submitButton.disabled = false;
 }
 
-function login(event){
-   event.preventDefault();
-   if (email.value == 'tryber@teste.com' && senha.value == '123456'){
+checkbox.addEventListener('click', checaBox);
+
+function login(event) {
+  event.preventDefault();
+  if (email.value === 'tryber@teste.com' && senha.value === '123456') {
     alert('Olá, Tryber!');
-    }
-    else {
-    alert('Email ou senha inválidos.')
-    }
+  } else {
+    alert('Email ou senha inválidos.');
+  }
 }
+buttonLogin.addEventListener('click', login);
 
 function count() {
-    let valor = textarea.value.length
-    let resultado = 500 - valor
-    contador.innerText = resultado
+  const valor = textarea.value.length;
+  const resultado = 500 - valor;
+  contador.innerText = resultado;
 }
-
+textarea.addEventListener('keyup', count);
+count();
